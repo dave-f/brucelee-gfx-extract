c282eb99e33d511f5e8107435395db7d546180d1
dave-f <dave.footitt@gmail.com>
2021-01-30 23:17:34 +0000

    Print out all pixel lookup tables

--- parsegfx.go
+++ parsegfx.go
@@ -27,6 +27,14 @@ func printGraphicsObject(name string, b []byte, pixelTable []byte, w int, h int,
 	}
 }
 
+func printPixelTable(b []byte) {
+	for i := 0; i<len(b); i++ {
+		l,r := decodePixel(b[i])
+		outputStr := fmt.Sprintf("%02x -> %02x / %02x", i, l, r)
+		fmt.Println(outputStr)
+	}
+}
+
 func main() {
 
 	if len(os.Args) != 3 {
@@ -53,20 +61,31 @@ func main() {
 	//fmt.Println(totalBytes, "total bytes")
 
 	// the graphics objects are in y order
-	printGraphicsObject("brick",data[10535:10535+(3*10)],data[7620:7620+16],3,10,false)
-	printGraphicsObject("hippo",data[10535:10535+(10*24)],data[7620:7620+16],10,24,true)
+	// printGraphicsObject("brick",data[10535:10535+(3*10)],data[7620:7620+16],3,10,false)
+	// printGraphicsObject("hippo",data[10535:10535+(10*24)],data[7620:7620+16],10,24,true)
 
-	/*
+	// 8 pixel lookup tables
+	pixTableOffs := 7620
+	pixTableSize := []int{16,16,16,4,4,4,4,4}
+
+	for i, e := range pixTableSize {
+		fmt.Println("Pixel table",i+1)
+		printPixelTable(data[pixTableOffs:pixTableOffs+e])
+		pixTableOffs += e
+		fmt.Println()
+	}
+
 	ptr := 8006 // lookup table offset
 
 	for i := 0; i < 59; i++ {
 		tableAddr := (uint16(data[ptr+1]) << 8) | uint16(data[ptr+0])
 		fileOffst := tableAddr + 4096 - 6400
-		outputStr := fmt.Sprintf("Object %02d : Data %04x (File offset %04d), Width %02d bytes (%02d pixels), Height %02d, Extra %02x", i, tableAddr, fileOffst, data[ptr+2], data[ptr+2]*2, data[ptr+3], data[ptr+4])
-		fmt.Println(outputStr)
+		if (i>0) {
+			outputStr := fmt.Sprintf("Object %02d : Data %04x (File offset %04d), Width %02d bytes (%02d pixels), Height %02d, Extra %02x", i, tableAddr, fileOffst, data[ptr+2], data[ptr+2]*2, data[ptr+3], data[ptr+4])
+			fmt.Println(outputStr)
+		}
 	    ptr += 5
 	}
-    */
 
 	// printing binary!
 	// dave := data[10535]
